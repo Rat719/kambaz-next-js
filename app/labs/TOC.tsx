0f79ffa3c99d29a7a876de18dd7bed62cc8e2552
chris <nipmucskater@gmail.com>
2026-04-19 21:40:52 -0400

    Updated server link

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -77,7 +77,7 @@ export default function TOC() {
         Node Server GitHub
       </a>
       <a
-        href="https://kambaz-node-server-app-a6-oms8.onrender.com"
+        href="https://kambaz-node-server-app-final-project-pjzf.onrender.com"
         target="_blank"
       >
         Node Server
